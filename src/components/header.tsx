00773840d0e168d345faf08a06c77a0b160b71a3
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2023-02-23 17:48:50 +0500

    updated

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -43,13 +43,13 @@ const { IsNavOpen, setIsNavOpen } = useContext(SettingsContext)
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
             className="w-7 h-7 text-white"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M6 18L18 6M6 6l12 12"
             />
           </svg>
