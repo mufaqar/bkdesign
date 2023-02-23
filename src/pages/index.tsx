0cbd0ce7626f25613617c0107818fd491bc2b121
Mufaqar Islam <mufaqar@gmail.com>
2023-02-23 14:24:50 +0500

    updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
-export default function Home() {
+
+export default function Index() {
   return (
     <h1 className="text-3xl font-bold underline">
       Hello world!
+
+      This is for me
     </h1>
   )
 }
