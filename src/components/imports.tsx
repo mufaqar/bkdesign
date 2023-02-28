531830f7b7a56f64f701862d700584b07528ac6b
syed zunair gillani <gillanix007@gmail.com>
2023-02-28 15:59:25 +0500

    remove error

--- src/components/imports.tsx
+++ src/components/imports.tsx
@@ -4,4 +4,3 @@ export {default as WhatWeOffer} from './what-we-offer';
 export {default as Projects} from './projects';
 export {default as Contectus} from './contectus';
 export {default as ProjectCrousel} from './projectCrousel';
-export {default as WhatwedoCrousel} from './whatwedoCrousel';
