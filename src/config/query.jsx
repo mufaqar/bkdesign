05f9c98d88f80faeeae54d9289729b61640f063c
Mufaqar Islam <mufaqar@gmail.com>
2025-05-25 13:51:16 +0500

    Update query.jsx

--- src/config/query.jsx
+++ src/config/query.jsx
@@ -2,7 +2,7 @@ export const partnersQuery = `*[_type == "partners"] | order(_createdAt asc) {
      image{
           asset->{
                url
-          },
+       },
           link
      }
      ,
